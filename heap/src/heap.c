7a7895f35aa6a461b78f22db3514f7019c408c55
Anish Sinha <anishsinha0128@gmail.com>
2022-05-05 13:41:36 -0400

    removed unnecessary line

--- heap/src/heap.c
+++ heap/src/heap.c
@@ -5,7 +5,6 @@ enum heap_stat heap(binary_heap_t *heap, size_t data_size, cmpfunc comparator, p
     heap->capacity   = 10;
     heap->size       = 0;
     heap->data       = malloc(sizeof(void *) * heap->capacity);
-    heap->max        = NULL;
     heap->comparator = comparator;
     heap->printer    = printer;
     heap->data_size  = data_size;
